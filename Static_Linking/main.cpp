c78eabc1b21638d8c179c60b826f28b1ef1ac5dd
Shubhangi47 <50401971+shubhangi47@users.noreply.github.com>
2023-07-24 20:52:01 +0530

    Add files via upload

---
+++ Static_Linking/main.cpp
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+#include <iostream>
+#include "calculation.h"
+int main()
+{
+    std::cout << "add :" << add(20, 10) << std::endl;
+    std::cout << "sub :" << sub(20, 10) << std::endl;
+    return 0;
+}
